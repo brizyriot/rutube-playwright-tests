cdae954d38cbbca82b8f3ed28080f96ef4137331
Виктория <v.dorzhieva@bnovo.ru>
2025-08-03 16:50:55 +0300

    main page tests

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -5,6 +5,12 @@ export class MainPage extends BasePage {
   private readonly headerLocator: Locator;
   private readonly categoriesTabsLocator: Locator;
   private readonly menuLocator: Locator;
+  private readonly headerAddButtonLocator: Locator;
+  private readonly headerNotificationsButtonLocator: Locator;
+  private readonly headrLoginButtonLocator: Locator;
+  private readonly headerAddButtonPopupListLocator: Locator;
+  private readonly headerNotificationsPopupLocator: Locator;
+  private readonly authorizationModalLocator: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -17,17 +23,56 @@ export class MainPage extends BasePage {
       })
       .nth(1);
     this.menuLocator = this.page.getByLabel('Облегченная панель навигации');
+    this.headerAddButtonLocator = this.page.getByRole('button', { name: 'Добавить' });
+    this.headerNotificationsButtonLocator = this.page.getByRole('button', { name: 'Уведомления' });
+    this.headrLoginButtonLocator = this.page.getByRole('button', { name: 'Вход и регистрация' });
+    this.headerAddButtonPopupListLocator = this.page.locator(
+      '.wdp-header-right-module__wrapper ul',
+    );
+    this.headerNotificationsPopupLocator = this.page.locator(
+      '.wdp-notifications-popup-module__wrapper ',
+    );
+    this.authorizationModalLocator = this.page
+      .locator('iframe[title="Multipass"]')
+      .contentFrame()
+      .locator("div[role='form']");
   }
   async open() {
     await this.page.goto('https://rutube.ru/');
   }
   async headerHasCorrectAriaSnapshot() {
-    await expect(this.headerLocator).toMatchAriaSnapshot();
+    await expect(this.headerLocator).toMatchAriaSnapshot({ name: 'headerAriaShapshot.yml' });
   }
   async categoriesTabsHasCorrectAriaSnapshot() {
-    await expect(this.categoriesTabsLocator).toMatchAriaSnapshot();
+    await expect(this.categoriesTabsLocator).toMatchAriaSnapshot({
+      name: 'categoriesTabsShapshot.yml',
+    });
   }
   async menuHasCorrectAriaSnapshot() {
-    await expect(this.menuLocator).toMatchAriaSnapshot();
+    await expect(this.menuLocator).toMatchAriaSnapshot({ name: 'menuShapshot.yml' });
+  }
+  async openAddPopupList() {
+    this.headerAddButtonLocator.click();
+  }
+  async openNotificationsPopup() {
+    this.headerNotificationsButtonLocator.click();
+  }
+  async openAuthorizationModal() {
+    this.headrLoginButtonLocator.click();
+  }
+  async addPopupListHasCorrectAriaShanshot() {
+    await expect(this.headerAddButtonPopupListLocator).toMatchAriaSnapshot({
+      name: 'addButtonPopupListShapshot.yml',
+    });
+  }
+  async notificationsPopupHasCorrectAriaShanshot() {
+    await expect(this.headerNotificationsPopupLocator).toMatchAriaSnapshot({
+      name: 'notificationsPopupShapshot.yml',
+    });
+  }
+  async authorizationModalHasCorrectAriaShanshot() {
+    await expect(this.authorizationModalLocator).toMatchAriaSnapshot({
+      name: 'authorizationModalShapshot.yml',
+    });
   }
 }
--- tests/specs/unauthorizaded/mainPage.spec.ts
+++ tests/specs/unauthorizaded/mainPage.spec.ts
@@ -4,11 +4,21 @@ import { MainPage } from '../../pages/MainPage';
 test('Проверка доступности элементов хедера', async ({ mainPage }) => {
   await mainPage.headerHasCorrectAriaSnapshot();
 });
-
 test('Проверка доступности элементов табов категорий', async ({ mainPage }) => {
   await mainPage.categoriesTabsHasCorrectAriaSnapshot();
 });
-
 test('Проверка доступности элементов бокового меню', async ({ mainPage }) => {
   await mainPage.menuHasCorrectAriaSnapshot();
+});
+test('Проверка доступности элементов списка добавления контента', async ({ mainPage }) => {
+  await mainPage.openAddPopupList();
+  await mainPage.addPopupListHasCorrectAriaShanshot();
+});
+test('Проверка доступности элементов попапа уведомления', async ({ mainPage }) => {
+  await mainPage.openNotificationsPopup();
+  await mainPage.notificationsPopupHasCorrectAriaShanshot();
+});
+test('Проверка доступности элементов модального окна авторизации', async ({ mainPage }) => {
+  await mainPage.openAuthorizationModal();
+  await mainPage.authorizationModalHasCorrectAriaShanshot();
 });
